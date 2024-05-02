5bd5f9bb6bf0d669b19bf0cd53886d61003b4bb7
Frontelli <matheusfrontelli@gmail.com>
2024-05-02 13:11:13 -0300

    Configurações de models para User e Categories

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -11,6 +12,18 @@ from django.utils import timezone
 
 
 # Create your models here.
+
+#django meta models
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -20,6 +33,16 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to= 'pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
