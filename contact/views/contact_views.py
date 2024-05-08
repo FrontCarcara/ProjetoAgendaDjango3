7fff06e38dc76bfb0be0fbc0cd629a8492189ea8
Frontelli <matheusfrontelli@gmail.com>
2024-05-08 11:04:26 -0300

    Configuracao de Template, View e Url para contato unico

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+
 from contact.models import Contact
+
 
 # Create your views here.
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects \
+        .filter (show=True) \
+        .order_by('-id')[10:20]
 
+    
     context = {
         'contacts': contacts
         }
@@ -12,5 +18,24 @@ def index(request):
     return render(
         request,
         'contact/index.html',
+        context,
+    )
+    
+def contact(request, contact_id):
+    #single_contact = Contact.objects.filter(id=contact_id).first()
+    #if single_contact is None:
+        #raise Http404()
+    
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+        )
+    
+    context = {
+        'contact': single_contact
+        }
+
+    return render(
+        request,
+        'contact/contact.html',
         context,
     )
