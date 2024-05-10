91d7e721d2614342b49209e47ddf2604f5170a2d
Frontelli <matheusfrontelli@gmail.com>
2024-05-10 09:21:48 -0300

    Mudança para forms.py do formulario

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,38 +1,10 @@
 from typing import Any
 
-from django import forms
-from django.core.exceptions import ValidationError
-from django.core.paginator import Paginator
 from django.db.models import Q
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import render
 
-from contact.models import Contact
+from contact.forms import ContactForms
 
-
-class ContactForms(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ('first_name', 'last_name', 'phone',)
-        
-    def clean(self) -> dict[str, Any]:
-        cleaned_data = self.cleaned_data
-
-        self.add_error(
-            None,
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )
-        self.add_error(
-            None,
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
-        
-        return super().clean()
 
 # Create your views here.
 def create(request):
