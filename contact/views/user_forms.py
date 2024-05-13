0cebdd71b57a24e086be9a6a5e4c1229f45d01db
Frontelli <matheusfrontelli@gmail.com>
2024-05-13 14:56:17 -0300

    Login e Logout

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,4 +1,5 @@
-from django.contrib import messages
+from django.contrib import auth, messages
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import redirect, render
 
 from contact.forms import RegisterForm
@@ -21,4 +22,31 @@ def register(request):
         {
             'form': form
         }
-    )    
+    )
+
+def login_view(request):
+    form = AuthenticationForm(request)
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+    
+        if form.is_valid():
+            user = form.get_user()
+            messages.success(request, 'Logado com sucesso!')
+            auth.login(request, user)
+            return redirect('contact:index')
+        messages.error(request, 'Usuário ou senha inválidos')
+    
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form
+        }
+    )
+    
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
+    
+    
