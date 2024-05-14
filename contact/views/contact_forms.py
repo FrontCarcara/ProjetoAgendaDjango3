3c767e7511c84652a2648877647c5bd64ff8a08f
Frontelli <matheusfrontelli@gmail.com>
2024-05-14 10:13:56 -0300

    Authenticação de login em contacts

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
@@ -5,6 +6,7 @@ from contact.forms import ContactForms
 from contact.models import Contact
 
 
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
 
@@ -37,7 +39,7 @@ def create(request):
         context
     )
 
-
+@login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
@@ -72,7 +74,7 @@ def update(request, contact_id):
         'contact/create.html',
         context
     )
-    
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
